3bb40613973aa928748f855d3ee195afc0e68bb5
dkhvam@gmail.com <Danny Hvam>
2016-10-31 11:29:13 +0100

    build cleanup

--- sample/src/main/java/com/shopgun/android/zoomlayout/sample/TextViewActivity.java
+++ sample/src/main/java/com/shopgun/android/zoomlayout/sample/TextViewActivity.java
@@ -3,6 +3,8 @@ package com.shopgun.android.zoomlayout.sample;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.shopgun.android.zoomlayout.ZoomLayout;
@@ -14,22 +16,38 @@ public class TextViewActivity extends AppCompatActivity {
     
     ZoomLayout mZoomLayout;
     TextView mTextView;
+    TextView mInfo;
+    boolean mHorizontal = true;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_textview);
 
         mZoomLayout = (ZoomLayout) findViewById(R.id.zoomLayout);
-        mTextView = (TextView) findViewById(R.id.info);
-        TextView mTv = (TextView) findViewById(R.id.textView);
-//        mTv.setText("x\nx\nx\nx\nx\nx\nx\nx\nx\nx\nx\nx\nx\nx\nx\nx\n");
+        mInfo = (TextView) findViewById(R.id.info);
+        mTextView = (TextView) findViewById(R.id.textView);
 
         // setup ZoomLayout
-        SimpleZoomPanLog log = new SimpleZoomPanLog(TAG, mTextView);
+        SimpleZoomPanLog log = new SimpleZoomPanLog(TAG, mInfo);
         log.setLogger(mZoomLayout);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(Menu.NONE, 1, Menu.NONE, "Change Layout");
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == 1) {
+            mHorizontal = !mHorizontal;
+            String text = mHorizontal ? "Hello, ZoomLayout!" : "H\ne\nl\nl\no\n,\n \nZ\no\no\nm\nL\na\ny\no\nu\nt\n!";
+            mTextView.setText(text);
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
