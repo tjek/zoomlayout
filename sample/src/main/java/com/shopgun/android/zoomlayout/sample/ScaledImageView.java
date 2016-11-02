f570ecee595ae3f8cc59bf1dd5639dcc74f0da54
dkhvam@gmail.com <Danny Hvam>
2016-11-02 08:48:22 +0100

    added a scaled imageview, in an attempt to provoke a bug allowing me to swipe the image while it's at a bound

---
+++ sample/src/main/java/com/shopgun/android/zoomlayout/sample/ScaledImageView.java
@@ -0,0 +1,51 @@
+package com.shopgun.android.zoomlayout.sample;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+public class ScaledImageView extends ImageView {
+    
+    public static final String TAG = ScaledImageView.class.getSimpleName();
+
+    float mImageAspectRatio;
+
+    public ScaledImageView(Context context) {
+        super(context);
+    }
+
+    public ScaledImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ScaledImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public void setImageAspectRatio(float imageAspectRatio) {
+        mImageAspectRatio = imageAspectRatio;
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+        float containerWidth = MeasureSpec.getSize(widthMeasureSpec);
+        float containerHeight = MeasureSpec.getSize(heightMeasureSpec);
+        float containerAspectRatio = containerWidth/containerHeight;
+
+//        L.d(TAG, String.format(Locale.US, "MeasureSpec[ w:%.0f, h:%.0f, containerAspectRatio:%.2f ], pageAspectRatio:%.2f",
+//                containerWidth, containerHeight, containerAspectRatio, mImageAspectRatio));
+
+        if (mImageAspectRatio < containerAspectRatio) {
+            containerWidth = containerHeight * mImageAspectRatio;
+        } else if (mImageAspectRatio > containerAspectRatio) {
+            containerHeight = containerWidth / mImageAspectRatio;
+        }
+        
+        setMeasuredDimension((int) containerWidth, (int) containerHeight);
+
+//        L.d(TAG, String.format(Locale.US, "Measured[ w:%s, h:%s ]", getMeasuredWidth(), getMeasuredHeight()));
+
+    }
+
+}
---
+++ sample/src/main/java/com/shopgun/android/zoomlayout/sample/ScaledImageViewActivity.java
@@ -0,0 +1,60 @@
+package com.shopgun.android.zoomlayout.sample;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.shopgun.android.zoomlayout.ZoomLayout;
+import com.shopgun.android.zoomlayout.ZoomOnDoubleTapListener;
+import com.shopgun.android.zoomlayout.sample.utils.Catalog;
+import com.shopgun.android.zoomlayout.sample.utils.SimpleZoomPanLog;
+
+public class ScaledImageViewActivity extends AppCompatActivity {
+
+    public static final String TAG = ScaledImageViewActivity.class.getSimpleName();
+    
+    ZoomLayout mZoomLayout;
+    TextView mTextView;
+    ScaledImageView mImageView;
+    Catalog mCatalog = Catalog.getNetto();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_scaled_imageview);
+
+        mZoomLayout = (ZoomLayout) findViewById(R.id.zoomLayout);
+        mTextView = (TextView) findViewById(R.id.info);
+        mImageView = (ScaledImageView) findViewById(R.id.imageViewScaled);
+
+        loadImageFromBitmap();
+
+        // setup ZoomLayout
+        SimpleZoomPanLog log = new SimpleZoomPanLog(TAG, mTextView);
+        log.setLogger(mZoomLayout);
+
+        mZoomLayout.setMinScale(1f);
+        mZoomLayout.setMaxScale(4f);
+        mZoomLayout.setOnDoubleTapListener(new ZoomOnDoubleTapListener(false));
+
+    }
+
+    private void loadImageFromBitmap() {
+
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inScaled = false;
+        Bitmap b = BitmapFactory.decodeResource(getResources(), R.drawable.boat, options);
+        float w = (float) b.getWidth();
+        float h = (float) b.getHeight();
+        float aspectRatio = w/h;
+//        L.d(TAG, String.format(Locale.US, "Bitmap[ %.0f, %.0f ], aspectRatio: %.2f", w, h, aspectRatio));
+        mImageView.setImageAspectRatio(aspectRatio);
+        mImageView.setImageBitmap(b);
+
+    }
+
+}
